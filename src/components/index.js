6109124dd6d772830c839d3ab80c3f3a3b938ad5
Ankit rajbhar <ankitrajbhar001@gmail.com>
2024-08-17 22:55:48 +0530

    compnents

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE";
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE
 }
