2e9ad043b48e4f5b4d0d7c73ed9efc5ffcf165d8
Ankit rajbhar <ankitrajbhar001@gmail.com>
2024-08-21 13:04:45 +0530

    pages

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { SignUp as SignUpComponenet} from "../components";
+
+
+function SignUp(){
+    return(
+        <div className="py-8">
+            <SignUpComponenet/>
+        </div>
+    )
+}
+
+export default SignUp
