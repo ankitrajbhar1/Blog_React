5bb259af6f2d06d4ce6a49a73a21c29202788f04
Ankit rajbhar <ankitrajbhar001@gmail.com>
2024-08-21 21:56:25 +0530

    final debug

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
-
+import conf from '../config/conf';
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
     return (
@@ -13,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                    apiKey='gffxizdo5mxjdkad884uj3mbisz0jy5k6530u5grhby81jyz'
+                    apiKey={conf.rteApiKey}
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
--- src/config/conf.js
+++ src/config/conf.js
@@ -3,7 +3,8 @@ const conf = {
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDBId: String(import.meta.env.VITE_APPWRITE_DB_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    rteApiKey: String(import.meta.env.VITE_RTE_API_KEY)
 }
 
 export default conf
